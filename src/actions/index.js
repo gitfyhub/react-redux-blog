4d128a6bafc791751711658bf565337a82802395
Faisal Younus <gitfyhub>
2020-01-27 08:16:06 -0600

    redux updates

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,43 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-    // Bad Approach
-    
-  const response = await jsonPlaceholder.get("/posts");
-
-  return {
-    type: "FETCH_POSTS",
-    payload: response
-  };
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log('about to fetch posts')
+  await dispatch(fetchPosts());
+  console.log('fetched posts')
 };
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch)
+// };
+
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
+
+// refactor of above lines 9 -16
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch)
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
+
+// this code works also using memoize - came up with this as a replacement to the above
+// export const fetchUser = _.memoize(function(id) {
+//   return _.memoize(async function(dispatch) {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+//   });
+// });
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postReducer";
+import usersReducer from "./userReducer";
 
 export default combineReducers({
-  replaceme: () => "999"
+  posts: postsReducer,
+  users: usersReducer
 });
