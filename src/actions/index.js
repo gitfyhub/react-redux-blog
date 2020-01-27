db844c84dd7f4c59f7015c880128a210719af578
Faisal Younus <gitfyhub>
2020-01-27 12:23:49 -0600

    single calls for users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,18 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async dispatch => {
-  console.log('about to fetch posts')
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log('fetched posts')
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // refactored the above commented code
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id=> dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async dispatch => {
